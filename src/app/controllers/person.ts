836f9ba21cd58a1a87c890c405b51637dbaaee3a
Rudhy Pereira <rudhy.costa@gcbinvestimentos.com>
2022-06-05 18:53:04 -0300

    refactor: :recycle: delete exemples files

--- src/app/controllers/person.ts
+++
@@ -1,6 +0,0 @@
-export class PersonController {
-  speak(name?: string): string {
-    return `Ola ${name?.toUpperCase ?? "Fulano"}!!!`;
-  }
-}
-
--- src/app/main/api/index.ts
+++
@@ -1,7 +0,0 @@
-import "../config/module-alias"; //aqui não pode importar usando o @
-
-import { PersonController } from "@/app/controllers/person";
-const p = new PersonController();
-
-p.speak("Rudhy");
-p.speak();
