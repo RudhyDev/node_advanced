aafb4a03356bb0a0117b10f77821748174a23cb1
Rudhy Pereira <rudhy.costa@gcbinvestimentos.com>
2022-06-29 19:10:59 -0300

    improve code quality

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -19,6 +19,7 @@ describe('FacebookApi', () => {
     httpClient.get
       .mockResolvedValueOnce({ access_token: 'any_app_token' })
       .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
+      .mockResolvedValueOnce({ id: 'any_fb_id', name: 'any_fb_name', email: 'any_fb_email' })
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
 
@@ -59,5 +60,17 @@ describe('FacebookApi', () => {
 
       }
     })
+  })
+
+  it('should return facebook user', async() => {
+    const fbUser = await sut.loadUser({ token: 'any_client_token' })
+
+    expect(fbUser).toEqual({
+      facebookId: 'any_fb_id',
+      name: 'any_fb_name',
+      email: 'any_fb_email'
+    })
+
+    return {}
   })
 })
